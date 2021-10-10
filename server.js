9c5ab95905d3ca0567ca2123f914a62b896bd373
smurphy7326 <smurphy7326@gmail.com>
2021-10-10 12:13:37 -0400

    12.2.3

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '',
+      password: 'Pass@7326',
       database: 'election'
     },
     console.log('Connected to the election database.')
